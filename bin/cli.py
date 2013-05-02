e5b6447ad7e2a227e358579ef49f517977fe91a1
Raghav Kashyap <raghavkashyap@paxterrasolutions.com>
2013-05-02 16:13:41 +0530

    Added License GPL V2 By Raghav Kashyap

--- bin/cli.py
+++ bin/cli.py
@@ -16,7 +16,8 @@ Created on 20-Dec-2012
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
-
+    You should have received a copy of the GNU General Public License
+    along with TestON.  If not, see <http://www.gnu.org/licenses/>.		
 
 
 '''
