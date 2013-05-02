658483081e51e1743b6f5f2f8afd7459dd569583
Raghav Kashyap <raghavkashyap@paxterrasolutions.com>
2013-05-02 16:15:01 +0530

    Added GPL V2 License by Raghav Kashyap

--- examples/StepParams/StepParams.py
+++ examples/StepParams/StepParams.py
@@ -15,7 +15,8 @@ Created on 28-Nov-2012
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
-
+    You should have received a copy of the GNU General Public License
+    along with TestON.  If not, see <http://www.gnu.org/licenses/>.		
 '''  
 class StepParams:
     ''' 
