3724b8b00e4c2eb78d0daf51ac84b83b9f033724
mishradeepak <dmsamishra@gmail.com>
2013-10-04 12:15:27 +0530

    Added SpirentDriver

--- drivers/common/api/stcdriver.py
+++ drivers/common/api/stcdriver.py
@@ -13,7 +13,7 @@
     You should have received a copy of the GNU General Public License
     along with TestON.  If not, see <http://www.gnu.org/licenses/>.	
     
-Basic Spirent stcdriver which will handle all basic stcdriver functions
+Basic Spirent stcdriver which will handle all basic stcdriver functions.
 '''
 import pexpect
 import struct
