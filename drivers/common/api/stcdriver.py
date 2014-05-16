afa143c9ebde657affc051c65b694fe5429f3297
Sudheervelivala <sudheer@paxterrasolutions.com>
2014-05-16 18:14:15 +0530

    Update stcdriver.py

--- drivers/common/api/stcdriver.py
+++ drivers/common/api/stcdriver.py
@@ -1,5 +1,6 @@
  #!/usr/bin/env python
  '''
+ @author:sudheer@paxterrasolutions.com,
   TestON is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 2 of the License, or
